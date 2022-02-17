e6850af1279ba17673be615fef9690baa38b583f
suraj200981 <suraj200981@yahoo.co.uk>
2022-02-17 10:41:28 +0000

    progress on recipe details component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,7 +11,9 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] =[
     new Recipe("A test recipe", "this is a description", "https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg" ),
+    new Recipe("A test recipe", "this is a description", "https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg" ),
     new Recipe("A test recipe", "this is a description", "https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg" )
+
 
   ]
   
