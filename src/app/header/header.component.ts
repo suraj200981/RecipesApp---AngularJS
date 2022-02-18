dba718492aaf6123763f24f6bf361a6b04c3c582
suraj200981 <suraj200981@yahoo.co.uk>
2022-02-18 14:38:12 +0000

    recipes and hopping list component displaying as pages but really are just individual components switching without page refresh

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
     selector: 'app-header',
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
 })
 
 export class HeaderComponent {
+
+  @Output() pageSelected = new EventEmitter<string>();
+
+    onSelect(page:string) {
+        
+        this.pageSelected.emit(page);
+    }
 
 }
