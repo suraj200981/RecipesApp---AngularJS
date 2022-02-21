415b93187b84d8cd3c6808bc83f937be06b12559
suraj200981 <suraj200981@yahoo.co.uk>
2022-02-21 14:38:16 +0000

    drop down in recipe details and dropdown in navbar working

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, HostBinding, HostListener } from '@angular/core';
+
+@Directive({
+    selector: '[appDropdown]'
+})
+
+export class DropdownDirective {
+    @HostBinding('class.open')  isOpen = false;
+
+    @HostListener('click') manageRecipeToggle() {
+        this.isOpen = !this.isOpen;
+    }
+
+}
