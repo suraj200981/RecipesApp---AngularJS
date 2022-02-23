738ecf912e7f7d4ba686a0de3df80788361f04de
suraj200981 <suraj200981@yahoo.co.uk>
2022-02-23 11:27:17 +0000

    ingredients added to example recipes in recipe service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 //managing recipes with recipes service
@@ -9,8 +10,23 @@ export class RecipeService {
 
     //you cannot directly access this array from the outside, unless you are using the getRecipes() method
     private recipes: Recipe[] =[
-        new Recipe("A test recipe", "this is a description", "https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg" ),
-        new Recipe("Fruit Salad", "Description for fruit salad", "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/pasta-salad-horizontal-jpg-1522265695.jpg" )
+        new Recipe("Chicken & halloumi burger", "Look no further than these chicken and halloumi burgers", "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/15-minute-chicken-halloumi-burgers-7dda022.jpg?quality=90&webp=true&resize=300,272" 
+        , [
+            new Ingredient("Skinless chicken breats", 2),
+            new Ingredient("Halloumi", 1),
+            new Ingredient("Bread", 1),
+            new Ingredient("Mustard", 1),
+            new Ingredient("Lettuce", 1),
+            new Ingredient("Tomato", 1),
+            new Ingredient("Bacon", 1)
+        ]),
+        new Recipe("Pan-fried salmon", "Have a perfectly cooked salmon fillet, complete with crisp skin...", "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/salmon-1547b3f.jpg?quality=90&webp=true&resize=300,272",
+        [
+            new Ingredient("Salmon", 1),
+            new Ingredient("Butter", 1),
+            new Ingredient("Green veg", 1),
+            new Ingredient("Seasoning", 1)
+        ])
       ];
 
     getRecipes(){
